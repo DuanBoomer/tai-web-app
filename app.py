18218647dd1fcea15ab86f2b0e1a53e47527dcc6
Chet25 <chetan25december@gmail.com>
2024-09-29 19:56:51 +0530

    hosting hotfix

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ st.title("Human Body based Virtual Controller")
 st.write("TAI (Tekken AI)")
 col1, col2 = st.columns([1, 1])
 
-################## Helper functions
+# Helper functions
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-pose = mp_pose.Pose(min_detection_confidence=0.2, min_tracking_confidence=0.2)
+pose = mp_pose.Pose(min_detection_confidence=0.8, min_tracking_confidence=0.5)
 
-# mp_hands = mp.solutions.hands
-# hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.8)
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.8)
 
 moves = ["right_punch", "lseft_punch", "left_kick", "right_kick"]
 move_mapping = {
@@ -41,7 +41,7 @@ stage = 'down'
 
 def press_key(s):
     #     if prev_key != s:
-    
+
     print('key pressed', s)
     keyboard.press(s)
 #     else: pass
@@ -79,72 +79,77 @@ def calculate_angle(a, b, c):
 
 # st.write("Hello world")
 with col1:
-    st.components.v1.iframe("https://www.retrogames.cc/embed/40238-tekken-3.html", width=600, height=450, scrolling=False)
+    st.components.v1.iframe(
+        "https://www.retrogames.cc/embed/40238-tekken-3.html", width=600, height=450, scrolling=False)
+
 
 def transform(frame: av.VideoFrame):
     global move, stage
     # with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     img = frame.to_ndarray(format="bgr24")
-    
+
     image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     image.flags.writeable = False
 
     # Make detection
     results = pose.process(image)
-    # hand_results = hands.process(image)
+    hand_results = hands.process(image)
+    # print(hand_results)
 
     # Recolor back to BGR
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    
-    
+
     try:
         landmarks = results.pose_landmarks.landmark
 
         # Left Arm
         left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                            landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                         landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
         left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-                        landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                      landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
         left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-                        landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
 
         # Right Arm
         right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-                            landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                          landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
         right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
-                        landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                       landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
         right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
-                        landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                       landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
 
         # Left Leg
         left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
                     landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
         left_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
-                        landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+                     landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
         left_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
-                        landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+                      landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
 
         # Right Leg
         right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
-                        landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                     landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
         right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
-                        landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+                      landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
         right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
-                        landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+                       landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
 
         # Upper body
-        mouth_left = [landmarks[mp_pose.PoseLandmark.MOUTH_LEFT.value].x,
-                        landmarks[mp_pose.PoseLandmark.MOUTH_LEFT.value].y]
-        mouth_right = [landmarks[mp_pose.PoseLandmark.MOUTH_RIGHT.value].x,
-                        landmarks[mp_pose.PoseLandmark.MOUTH_RIGHT.value].y]
+        # mouth_left = [landmarks[mp_pose.PoseLandmark.MOUTH_LEFT.value].x,
+        #               landmarks[mp_pose.PoseLandmark.MOUTH_LEFT.value].y]
+        # mouth_right = [landmarks[mp_pose.PoseLandmark.MOUTH_RIGHT.value].x,
+        #                landmarks[mp_pose.PoseLandmark.MOUTH_RIGHT.value].y]
 #             nose = [landmarks[mp_pose.PoseLandmark.NOSE.value].x,landmarks[mp_pose.PoseLandmark.NOSE.value].y]
         left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                            landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                         landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
         right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-                            landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-        mid_shoulder = [(right_shoulder[0] + left_shoulder[0]) / 2,
-                        (right_shoulder[1] + left_shoulder[1]) / 2]
+                          landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+        # mid_shoulder = [(right_shoulder[0] + left_shoulder[0]) / 2,
+        #                 (right_shoulder[1] + left_shoulder[1]) / 2]
+
+        # Hand models
+        # right hand
 
         # Calulate distance
         right_shoulder_left_wrist_dist = calculate_dist(
@@ -161,10 +166,10 @@ def transform(frame: av.VideoFrame):
         angle_right_leg = calculate_angle(
             right_hip, right_knee, right_ankle)
 
-        angle_mid_left_shoulder = calculate_angle(
-            mouth_left, mid_shoulder, left_shoulder)
-        angle_mid_right_shoulder = calculate_angle(
-            mouth_right, mid_shoulder, right_shoulder)
+        # angle_mid_left_shoulder = calculate_angle(
+        #     mouth_left, mid_shoulder, left_shoulder)
+        # angle_mid_right_shoulder = calculate_angle(
+        #     mouth_right, mid_shoulder, right_shoulder)
 
 #             angle_mid_left_shoulder = calculate_angle(nose, mid_shoulder, left_shoulder)
 #             angle_mid_right_shoulder = calculate_angle(nose, mid_shoulder, right_shoulder)
@@ -179,10 +184,10 @@ def transform(frame: av.VideoFrame):
         cv2.putText(image, str(round(angle_right_leg, 2)), tuple(np.multiply(right_knee, [
                     640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
-        cv2.putText(image, str(round(angle_mid_left_shoulder, 2)), tuple(np.multiply(left_shoulder, [
-                    640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(image, str(round(angle_mid_right_shoulder, 2)), tuple(np.multiply(right_shoulder, [
-                    640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+        # cv2.putText(image, str(round(angle_mid_left_shoulder, 2)), tuple(np.multiply(left_shoulder, [
+        #             640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+        # cv2.putText(image, str(round(angle_mid_right_shoulder, 2)), tuple(np.multiply(right_shoulder, [
+        # 640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
         cv2.putText(image, str(round(right_shoulder_left_wrist_dist, 2)), tuple(np.multiply(left_wrist, [
                     640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
@@ -195,25 +200,25 @@ def transform(frame: av.VideoFrame):
             stage = 'down'
 
         # Upper Body (Head movement)
-        elif angle_mid_left_shoulder < 70:
-            stage = "head_left_tilt"
+#         elif angle_mid_left_shoulder < 70:
+#             stage = "head_left_tilt"
 
-        elif angle_mid_left_shoulder >= 80 and stage == "head_left_tilt":
-            move = "left_head_tilt"
-            key = Key.left
-#                 print("left arrow")
-            press_key(key)
-            stage = "down"
+#         elif angle_mid_left_shoulder >= 80 and stage == "head_left_tilt":
+#             move = "left_head_tilt"
+#             key = Key.left
+# #                 print("left arrow")
+#             press_key(key)
+#             stage = "down"
 
-        elif angle_mid_right_shoulder < 70:
-            stage = "head_right_tilt"
+#         elif angle_mid_right_shoulder < 70:
+#             stage = "head_right_tilt"
 
-        elif angle_mid_right_shoulder >= 80 and stage == "head_right_tilt":
-            move = "right_head_tilt"
-            key = Key.right
-#                 print("right arrow")
-            press_key(key)
-            stage = "down"
+#         elif angle_mid_right_shoulder >= 80 and stage == "head_right_tilt":
+#             move = "right_head_tilt"
+#             key = Key.right
+# #                 print("right arrow")
+#             press_key(key)
+#             stage = "down"
 
         # Upper body (jump) drpawancse@gmail.com
         elif left_shoulder_right_wrist_dist < 0.20 and right_shoulder_left_wrist_dist < 0.20:
@@ -269,19 +274,90 @@ def transform(frame: av.VideoFrame):
             stage = "down"
     except:
         pass
-    
+
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(
-                                    color=(245, 117, 66), thickness=2, circle_radius=2),
-                                mp_drawing.DrawingSpec(
-                                    color=(245, 66, 230), thickness=2, circle_radius=2)
-                                )
-    
-    # if hand_results.multi_hand_landmarks:
-    # for hand_landmarks in results.multi_hand_landmarks:
-    #     mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-    
-    
+                              mp_drawing.DrawingSpec(
+                                  color=(245, 117, 66), thickness=2, circle_radius=2),
+                              mp_drawing.DrawingSpec(
+                                  color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
+    # print('hand_results', hand_results.multi_hand_landmarks)
+    if hand_results.multi_hand_landmarks:
+        for hand_landmarks in hand_results.multi_hand_landmarks:
+            #         handIndex = hand_results.multi_hand_landmarks.index(hand_landmarks)
+            #         handLabel = hand_results.multi_handedness[handIndex].classification[0].label
+            #         print(handLabel)
+
+            #         handLandmarks = []
+
+            #         left_hand_angle = 0
+            #         # right_hand_angle = 0
+
+            #         # Fill list with x and y positions of each landmark
+            #         for landmarks in hand_landmarks.landmark:
+            #             handLandmarks.append([landmarks.x, landmarks.y])
+
+            #         # print(handLandmarks)
+
+            #         if handLabel == "Right":
+            #             left_hand_angle = calculate_angle(handLandmarks[14], handLandmarks[15], handLandmarks[16])
+            #             print('left', left_hand_angle)
+
+            #             if left_hand_angle > 140:
+            #                 press_key(Key.right)
+
+            # if handLabel == "Right":
+            #     right_hand_angle = calculate_angle(handLandmarks[14], handLandmarks[15], handLandmarks[16])
+
+            #     if right_hand_angle > 80:
+            #         press_key(Key.right)
+            #     left_hand_fingers = left_hand_fingers+1
+
+            # Thumb
+            # if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
+            #     left_hand_fingers = left_hand_fingers+1
+            # # Index finger
+            # if handLabel == "Left" and handLandmarks[8][0] < handLandmarks[6][0]:
+            #     left_hand_fingers = left_hand_fingers+1
+            # # Middle finger
+            # if handLabel == "Left" and handLandmarks[12][0] < handLandmarks[10][0]:
+            #     left_hand_fingers = left_hand_fingers+1
+            # # Ring finger
+            # if handLabel == "Left" and handLandmarks[16][0] < handLandmarks[14][0]:
+            #     left_hand_fingers = left_hand_fingers+1
+            # # Pinky
+            # if handLabel == "Left" and handLandmarks[20][0] < handLandmarks[18][0]:
+            #     left_hand_fingers = left_hand_fingers+1
+
+            # if left_hand_fingers == 5:
+            #     stage = 'right_head_tilt'
+            #     press_key(Key.left)
+            #     print('backward')
+
+            # if handLabel == "Right" and handLandmarks[4][1] > handLandmarks[3][1]:
+            #     right_hand_fingers = right_hand_fingers+1
+            # # Index finger
+            # if handLabel == "Right" and handLandmarks[8][1] < handLandmarks[6][1]:
+            #     right_hand_fingers = right_hand_fingers+1
+            # # Middle finger
+            # if handLabel == "Right" and handLandmarks[12][1] < handLandmarks[10][1]:
+            #     right_hand_fingers = right_hand_fingers+1
+            # # Ring finger
+            # if handLabel == "Right" and handLandmarks[16][1] < handLandmarks[14][1]:
+            #     right_hand_fingers = right_hand_fingers+1
+            # # Pinky
+            # if handLabel == "Right" and handLandmarks[20][1] < handLandmarks[18][1]:
+            #     right_hand_fingers = right_hand_fingers+1
+
+            # if right_hand_fingers == 5:
+            #     stage = 'left_head_tilt'
+            #     press_key(Key.right)
+            #     print('forward')
+
+            mp_drawing.draw_landmarks(
+                image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
     return av.VideoFrame.from_ndarray(image, format="bgr24")
 
 
@@ -290,7 +366,7 @@ with col2:
         key="streamer",
         video_frame_callback=transform,
         sendback_audio=False
-        )
+    )
 # <iframe
 #     id="tekkenWindow"
 # 			src=
@@ -302,4 +378,3 @@ with col2:
 # 			mozallowfullscreen="true"
 # 			scrolling="no"
 # 		></iframe>
-
